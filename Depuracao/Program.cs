8ec408444bf1716c4660de549aa2923e9daf959e
Rafael R. Costa <phaelrlz@gmail.com>
2012-08-16 21:34:14 -0300

    Adicionei a Divisao.

--- Depuracao/Program.cs
+++ Depuracao/Program.cs
@@ -12,6 +12,11 @@ namespace Depuracao
             N = N + 1;
             return N;
         }
+
+        static float DIVIDIR(int N, int D)
+        {
+            return N / D;
+        }
         static void Main(string[] args)
         {
             int NUMERO = 0;
@@ -20,7 +25,17 @@ namespace Depuracao
             Console.WriteLine("O Numero Escolhido eh : {0}", NUMERO);
             NUMERO = MUDARNUMERO(NUMERO);
             Console.WriteLine("O Numero Modificado : {0} ", NUMERO);
+            
+            int DENOMINADOR = 0;
+
+            DENOMINADOR = int.Parse(Console.ReadLine());
+            if (DENOMINADOR == 0)
+                Console.WriteLine("Nao eh Possivel dividir por zero");
+            else
+                Console.WriteLine("O quociente entre {0} e {1} eh : {2}", NUMERO, DENOMINADOR, DIVIDIR(NUMERO, DENOMINADOR));
+
             Console.Read();
+            
         }
     }
 }
